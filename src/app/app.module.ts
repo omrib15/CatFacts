3d11f6689e84f8c0d4cf2abea211631c4b460684
Omri Basch <omrib15@gmail.com>
2019-12-19 19:54:13 +0200

    moved components into directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RouterModule, Routes } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { AllFactsComponent } from './all-facts/all-facts.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { AllFactsComponent } from './components/all-facts/all-facts.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { AllFactsComponent } from './all-facts/all-facts.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatTabsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       
       { path: 'all', component: AllFactsComponent },
--- src/app/components/all-facts/all-facts.component.ts
+++ src/app/components/all-facts/all-facts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FactsService } from '../../services/facts.service';
 
 @Component({
   selector: 'app-all-facts',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllFactsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private factsService:FactsService) { }
 
   ngOnInit() {
   }
