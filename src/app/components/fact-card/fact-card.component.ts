27175dd66ba01a06b8afbb8adec59bc3d11e9409
Omri Basch <omrib15@gmail.com>
2019-12-20 22:15:33 +0200

    added fact-card text property

--- src/app/components/fact-card/fact-card.component.ts
+++ src/app/components/fact-card/fact-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fact-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fact-card.component.css']
 })
 export class FactCardComponent implements OnInit {
+  @Input() text;
 
   constructor() { }
 
