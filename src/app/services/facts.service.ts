891698ca82b59a24b182204ae11e2b15162e0e86
Omri Basch <omrib15@gmail.com>
2019-12-21 14:28:29 +0200

    facts service handles all fact traffic now

--- src/app/services/facts.service.ts
+++ src/app/services/facts.service.ts
@@ -8,16 +8,45 @@ import { Observable } from 'rxjs';
 export class FactsService {
   factsUrl:string = 'http://localhost:3000/api/facts';
   allFacts;
+  nextFactIndex;
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.nextFactIndex = 0;
+   }
 
-  getAllFacts():Observable<any>{
-    return this.http.get(this.factsUrl)
+
+  initService(){
+    this.http.get(this.factsUrl).subscribe((data) => {
+        this.allFacts = data['all'];
+      }
+    )
   }
 
-  getFactsText(){
-    const texts = this.allFacts.map(fact => fact['text']);
-    return texts;
+  // getAllFacts():Observable<any>{
+  //   return this.http.get(this.factsUrl);
+  // }
+
+  // updateAllFacts(all){
+  //   this.allFacts = all;
+  // }
+
+  // getFactsText(){
+  //   const texts = this.allFacts.map(fact => fact['text']);
+  //   return texts;
+  // }
+
+  getNextFact(){
+    const fact = this.allFacts[this.nextFactIndex++];
+    this.nextFactIndex = this.nextFactIndex % this.allFacts.length;
+    return fact;
+  }
+
+  getPrevFact(){
+    const fact = this.allFacts[this.nextFactIndex--];
+    if(this.nextFactIndex < 0 ){
+      this.nextFactIndex = this.allFacts.length - 1;
+    }
+    return fact;
   }
 
 }
