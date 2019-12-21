686f08e97a411384116d0bffac1dfbb6f2cee435
Omri Basch <omrib15@gmail.com>
2019-12-21 14:27:47 +0200

    next/prev functionality improved

--- src/app/components/fact-card/fact-card.component.ts
+++ src/app/components/fact-card/fact-card.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FactsService } from '../../services/facts.service';
+import { Fact } from 'src/app/models/fact';
+
+/*
+  This component is currently used to implement the carousel functionality,
+  In the future I will wrap this fact-card component with a "carousel-component"
+  which be in charge of the carousel functionality
+*/
 
 @Component({
   selector: 'app-fact-card',
@@ -7,23 +15,45 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FactCardComponent implements OnInit {
   @Input() text: string;
+  fact: Fact;
   imgNumber: number ;
   imgName: string ;
   imgCount: number;
 
-  constructor() {
+  constructor(private factsService: FactsService) {
     this.imgNumber = 1;
     this.imgName = `cat${this.imgNumber}.jpg`;
     this.imgCount = 10;
+    this.fact = factsService.getNextFact();
+    
    }
 
   ngOnInit() {
   }
 
+  next(){
+    console.log("next clicked");
+    this.nextFact();
+    this.nextImage();
+  }
+
+  prev(){
+    console.log("prev clicked");
+    this.prevFact();
+    this.prevImage();
+  }
+
+  nextFact(){
+    this.fact = this.factsService.getNextFact();
+  }
+
+  prevFact(){
+    this.fact = this.factsService.getPrevFact();
+  }
+
   nextImage(){
     this.imgNumber = (this.imgNumber + 1) % this.imgCount;
     this.imgName = `cat${this.imgNumber}.jpg`;
-    console.log(this.imgNumber);
   }
 
   prevImage(){
@@ -32,7 +62,6 @@ export class FactCardComponent implements OnInit {
       this.imgNumber = this.imgCount - 1;
     }
     this.imgName = `cat${this.imgNumber}.jpg`;
-    console.log(this.imgNumber);
   }
 
 }
