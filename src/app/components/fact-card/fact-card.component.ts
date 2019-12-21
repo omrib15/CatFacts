0eb1978498c7350da0c8b2ada52672a724e97bb9
Omri Basch <omrib15@gmail.com>
2019-12-21 13:06:09 +0200

    added fact-card next/prev functionality

--- src/app/components/fact-card/fact-card.component.ts
+++ src/app/components/fact-card/fact-card.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FactCardComponent implements OnInit {
   @Input() text: string;
+  imgNumber: number ;
+  imgName: string ;
+  imgCount: number;
 
-  constructor() { }
+  constructor() {
+    this.imgNumber = 1;
+    this.imgName = `cat${this.imgNumber}.jpg`;
+    this.imgCount = 10;
+   }
 
   ngOnInit() {
+  }
+
+  nextImage(){
+    this.imgNumber = (this.imgNumber + 1) % this.imgCount;
+    this.imgName = `cat${this.imgNumber}.jpg`;
+    console.log(this.imgNumber);
+  }
+
+  prevImage(){
+    this.imgNumber--;
+    if(this.imgNumber < 0 ){
+      this.imgNumber = this.imgCount - 1;
+    }
+    this.imgName = `cat${this.imgNumber}.jpg`;
+    console.log(this.imgNumber);
   }
 
 }
